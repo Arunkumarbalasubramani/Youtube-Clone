7355d24cbc16426626027a92a39e2040387af78f
Arunkumarbalasubramani <arunkumar.vbs.b@gmail.com>
2022-10-09 18:03:36 +0530

    Navbar Creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  Feed,
+  VideoDetails,
+  ChannelDetails,
+  SearchFeed,
+  Navbar,
+} from "./Components/export";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Youtube Clone</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
+          <Route path="/channel/:id" element={<ChannelDetails />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
